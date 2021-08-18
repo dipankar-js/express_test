ace6289859674cc278570374667cb40f2606236f
Dipankar Barman <barman498@gmail.com>
2021-08-18 17:55:13 +0530

    added comments

--- test.js
+++ test.js
@@ -9,3 +9,6 @@
 // 1. initialise git --> git init
 // 2. Add files ----> git add <filename> ... add all file ---> git add .
 // 3. Check Status  ---> git status
+// 4. commit your changes ---> git commit -m "commit message"
+// 5. connect to your remote repo (only for 1st time);
+// 6. Push your changes to Github  --> git push
